f5f048eed6493666b68c9136eb305fc8634e63b5
zaighamimtiaz <zaighamimtiaz@gmail.com>
2017-01-21 00:57:12 +0500

    basic servise created

--- app/src/main/java/com/example/hassan/servicetest/MainActivity.java
+++ app/src/main/java/com/example/hassan/servicetest/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.hassan.servicetest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(getApplicationContext(),MyService.class);
+                        startService(i);
+                    }
+                }
+        );
+
+        btn2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(getApplicationContext(),MyService.class);
+                    }
+                }
+        );
     }
 }
