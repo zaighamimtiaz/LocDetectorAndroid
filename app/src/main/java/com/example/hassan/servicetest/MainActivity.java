8a6137928bd05eaceb4ecc8aca6034c8a0b1d06e
zaighamimtiaz <zaighamimtiaz@gmail.com>
2017-01-21 03:40:06 +0500

    getting gps coordinates via service

--- app/src/main/java/com/example/hassan/servicetest/MainActivity.java
+++ app/src/main/java/com/example/hassan/servicetest/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.hassan.servicetest;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +17,35 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1,btn2;
+    private Button btn1,btn2;
+    private BroadcastReceiver broadcastReceiver;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (broadcastReceiver == null){
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+
+                    String str = intent.getExtras().getString("coordinates");
+
+                    Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
+                }
+            };
+        }
+
+        registerReceiver(broadcastReceiver,new IntentFilter("location_update"));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (broadcastReceiver != null)
+            unregisterReceiver(broadcastReceiver);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +54,16 @@ public class MainActivity extends AppCompatActivity {
 
         btn1 = (Button)findViewById(R.id.btn1);
         btn2 = (Button)findViewById(R.id.btn2);
+
+
+
+
+
+        if(!runtime_permissions())
+            enable_buttons();
+    }
+
+    private void enable_buttons() {
 
         btn1.setOnClickListener(
                 new View.OnClickListener() {
@@ -41,5 +87,34 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+    }
+
+    private boolean runtime_permissions() {
+
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                this,Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION ,
+            Manifest.permission.ACCESS_COARSE_LOCATION},100);
+
+            return  true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                grantResults[1] == PackageManager.PERMISSION_GRANTED)
+            enable_buttons();
+
+        else
+            runtime_permissions();
     }
 }
--- app/src/main/java/com/example/hassan/servicetest/MyService.java
+++ app/src/main/java/com/example/hassan/servicetest/MyService.java
@@ -1,10 +1,24 @@
 package com.example.hassan.servicetest;
 
+import android.app.LoaderManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.audiofx.BassBoost;
+import android.media.audiofx.EnvironmentalReverb;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.Settings;
+import android.widget.Toast;
 
 public class MyService extends Service {
+
+    private LocationListener listener;
+    private LocationManager locationManager;
+
     public MyService() {
     }
 
@@ -12,5 +26,57 @@ public class MyService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                Intent i = new Intent("location_update");
+
+                String lat = String.valueOf(location.getLatitude());
+                String lon = String.valueOf(location.getLongitude());
+
+                i.putExtra("coordinates", "lat : " + lat + " " + "lon : " + lon);
+
+                sendBroadcast(i);
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        };
+
+        locationManager = (LocationManager)getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+
+        //noinspection MissingPermission
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,3000,0,listener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (locationManager != null)
+            //noinspection MissingPermission
+            locationManager.removeUpdates(listener);
     }
 }
